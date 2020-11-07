e756ffdac99fc78974066ef699aa6e84ea6e6263
IgnacioDCordoba <63248914+Ignacio1997-prog@users.noreply.github.com>
2020-11-07 16:34:15 -0300

    Revert "View DB"
    This reverts commit 4e1aa2c587f91c51f7e4e99264291a1e0fcbeb90.

---
+++ TPC_Cordoba_Keegan/Dominio/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Cliente
+    {
+        public int id;
+        public string Nombre{ get; set; }
+        public string Apellido { get; set; }
+        public decimal Precio { get; set; }
+        public string FechaAlta{ get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+    }
+}
